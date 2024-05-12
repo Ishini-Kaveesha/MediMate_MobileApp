9b784662fd86a00f9c651d6db804d46f5642a503
IshiniKaveesha <ishinikaveesha84@gmail.com>
2024-05-13 00:29:25 +0530

    Create first page

--- app/src/main/java/com/example/crud_app/MainActivity.kt
+++ app/src/main/java/com/example/crud_app/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.crud_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var logoImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        logoImage= findViewById(R.id.logoimage)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.regMedPage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        logoImage.setOnClickListener {
+            val intent = Intent(this@MainActivity, ShowMedication::class.java)
+            startActivity(intent)
         }
     }
 }
